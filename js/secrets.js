1aab56dc2acb2369b3294b5c0969a829cdf073e2
Winny <158020790+winnykung02@users.noreply.github.com>
2024-05-20 17:18:33 -0700

    water drop sound

--- js/secrets.js
+++ js/secrets.js
@@ -1,7 +1,3 @@
-
-    
-
-
 // //click on background changes text color, looped between pink & dark gray//
 // // box.onclick = function () {
 // //     document.getElementById("textColor").style.color,
@@ -13,3 +9,146 @@
 // //         box.style.color = "pink";
 
 // //     box.style.textDecorationColor = textColor;}
+
+
+let circles;
+let spots;
+let img;
+let canCreateCircles = true; // controls creation of circles
+let observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            // start the animation when element is visible
+            loop();
+            // stop observing
+            observer.unobserve(entry.target);
+        }
+    });
+}, {
+    threshold: 0.1
+});
+
+// observe the canvas container
+observer.observe(document.querySelector('.canvas-container'));
+
+function preload() {
+    img = loadImage("media/2050.png");
+}
+
+function setup() {
+    let canvas = createCanvas(900, 400);
+    canvas.parent('the-canvas');
+    img.loadPixels();
+    spots = [];
+    circles = [];
+    for (let x = 0; x < img.width; x++) {
+        for (let y = 0; y < img.height; y++) {
+            let index = x + y * img.width;
+            let c = img.pixels[index * 4];
+            let b = brightness([c]);
+            if (b > 1) {
+                spots.push(createVector(x, y));
+            }
+        }
+    }
+    noLoop(); // Ensure drawing is paused initially
+}
+
+setTimeout(() => {
+    canCreateCircles = false;
+}, 12000);
+
+function draw() {
+    background(0);
+
+    let total = 5;
+    let count = 0;
+    let attempts = 0;
+
+    while (count < total && canCreateCircles) {
+        let newC = newCircle();
+        if (newC !== null) {
+            circles.push(newC);
+            count++;
+        }
+        attempts++;
+        if (attempts > 500) {
+            noLoop();
+            break;
+        }
+    }
+
+    for (let i = 0; i < circles.length; i++) {
+        let circl = circles[i];
+
+        if (circl.growing) {
+            if (circl.edges()) {
+                circl.growing = false;
+            } else {
+                for (let j = 0; j < circles.length; j++) {
+                    let other = circles[j];
+                    if (circl !== other) {
+                        var d = dist(circl.x, circl.y, other.x, other.y);
+                        var distance = circl.r + other.r;
+
+                        if (d - 1 < distance) {
+                            circl.growing = false;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        circl.show();
+        circl.grow();
+    }
+}
+
+function newCircle() {
+    var r = int(random(0, spots.length));
+    var spot = spots[r];
+    var x = spot.x;
+    var y = spot.y;
+
+    var valid = true;
+    for (var i = 0; i < circles.length; i++) {
+        var circle = circles[i];
+        var d = dist(x, y, circle.x, circle.y);
+        if (d < circle.r) {
+            valid = false;
+            break;
+        }
+    }
+    if (valid) {
+        return new Circle(x, y);
+    } else {
+        return null;
+    }
+}
+
+class Circle {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.r = 1;
+        this.growing = true;
+    }
+
+    grow() {
+        if (this.growing) {
+            this.r += 1;
+        }
+    }
+
+    edges() {
+        return (this.x + this.r > width || this.x - this.r < 0 || this.y + this.r > height || this.y - this.r < 0);
+    }
+
+    show() {
+        stroke(255);
+        strokeWeight(1);
+        noFill();
+        ellipse(this.x, this.y, this.r * 2);
+    }
+}
--- js/sound.js
+++ js/sound.js
@@ -1,4 +1,4 @@
-let sounds = [new Audio('media/ding-triangle-hit-smartsound-fx-1-00-03.mp3'), new Audio('media/ding-triangle-hit-smartsound-fx-1-00-03 (mp3cut.net).mp3'), new Audio('media/hotel-bell-ding-1-174457.mp3')];
+let sounds = [new Audio('media/water-drop-plop.mp3'), new Audio('media/water-drop-plop.mp3'), new Audio('media/water-drop-plop.mp3')];
 let lastSound = 0;
 
 
@@ -20,8 +20,8 @@ sounds[lastSound].pause();
 
 song.volume = 0.3;
 song.loop = true;
-sounds.volume = 0.1;
-// sounds.volume = 0.1;
+sound.volume = 0.15;
+
 
 playBtn.addEventListener("mousedown", button_action);
 
@@ -56,15 +56,3 @@ sound.addEventListener("click", click_sfx);
 function click_sfx() {
     sound.play();
 }
-
-
-// let sound = document.querySelector("#sound");
-// var sound = new Audio("#sound");
-// sound.preload = 'auto';
-// sound.load();
-
-// function playSound(volume: 0.2) {
-//   var click=sound.cloneNode();
-//   click.volume=volume;
-//   click.play();
-// }
